c24afdde00985bddf933b5cda5204dabc4defe80
Sandeep <>
2020-04-24 23:31:45 +0530

    my test java commit

---
+++ Hello.java
@@ -0,0 +1,18 @@
+package com.cks;
+
+import java.util.Date;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		byte age = 37;
+		byte myAge = age;
+		boolean isEligible = false;
+		System.out.println("Hello World. This is my first Java Code.");
+		System.out.println(myAge);
+		System.out.println(isEligible);
+		Date now = new Date();
+		System.out.println(now);
+	}
+}
+
